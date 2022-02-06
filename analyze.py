1ad22e4b3bc503d759682a3468f9eef2cd4fa8d1
Ethan Read <ethan.read@gmail.com>
2022-02-06 13:05:34 -0500

    .

--- analyze.py
+++ analyze.py
@@ -7,76 +7,93 @@ import pandas as pd
 import datetime
 
 
+def analyze_sample(url: str, data: pd.DataFrame, row: int) -> None:
+    """
+    Updates one row in <data> with the comment averages.
+    """
+    response = requests.get(url)
+    if response.status_code !=200:  # Server eror, try one more time
+        time.sleep(0.5)
+        response = requests.get(url)
+        if response.status_code != 200:
+            print(f"Request failed, status code {response.status_code}")
+            data[row][3] = response.status_code
+            return
 
-def get_averages(response: dict) -> tuple:
-    """
-    Returns the average character length of the comments and a crude approximation
-    of the average word length
-    """
-    comments = response['data']
+    comments = response.json()['data']
     total_chars = 0
     total_spaces = 1
     for comment in comments:
         total_chars += len(comment['body'])
         total_spaces += comment['body'].count(' ')
 
-    return total_chars / len(comments), total_chars / total_spaces
+    com_length = total_chars / len(comments)
+    word_length = total_chars / total_spaces
+
+    data[row][1] = com_length
+    data[row][2] = word_length
+    data[row][3] = response.status_code
+    data[row][4] = len(comments)
 
 
 def analyze(start_time: int, subreddit: str, sample_size: int,
-            end_time=int(time.time())) -> pd.DataFrame:
+            end_time=int(time.time()), daily_offset=30000, small_sample_regime=0) -> pd.DataFrame:
+    """
+    Returns a DataFrame with daily average comment and word length.
 
+    start_time: A UTC timestamp that determines the starting point of the analysis.
+
+    sample_size: The number of comments sampled each day.
+
+    daily_offset: A random number of seconds are added to the daily sampling time to
+    avoid sampling bias. This parameter is the upper bound for the offset.
+
+    small_sample_regime: Specifies a number of days for which the daily offset is 0
+    and the program limits itself to 24 hours of comments. Useful if a subreddit grows quickly.
+    """
     if start_time >= end_time:
         raise ValueError("start_time must be smaller than end_time")
     days = (end_time - start_time) // 86400
-    data = np.zeros((days, 4))
+    data = np.zeros((days, 5))
 
     for i in range(days):
         print(i)
-        daily_time = start_time + i * 86400 + randint(0, 50000)
+        daily_start_time = start_time + i * 86400 + randint(0, 30000)
         url = 'https://api.pushshift.io/reddit/search/comment/' + \
-        f'?subreddit={subreddit}&after={daily_time}&fields=body&size={sample_size}'
-        comments = requests.get(url)
-
-        if comments.status_code !=200:  # Server eror, try one more time
-            time.sleep(0.5)
-            comments = requests.get(url)
-            if comments.status_code != 200:
-                print(f"Request failed, status code {comments.status_code}")
-                com_length = word_length = 0
-        else:
-            com_length, word_length = get_averages(comments.json())
-
-
-        data[i][0] = daily_time
-        data[i][1] = com_length
-        data[i][2] = word_length
-        data[i][3] = comments.status_code
-        time.sleep(0.5)  # The server has a limit of 120 requests/minute
+        f'?subreddit={subreddit}&after={daily_start_time}&fields=body&size={sample_size}'
+        if i < small_sample_regime:
+            daily_end_time = daily_start_time + 86400
+            extra = f'&before={daily_end_time}'
+            url += extra
+        data[i][0] = daily_start_time
+        analyze_sample(url, data, i)
+        time.sleep(0.5)  # The pushshift api has a limit of 120 requests/minute
 
     results = pd.DataFrame(data, columns=
                            ['Retrieval Time', 'Comment Length',
-                            'Word Length', 'Response Code'])
+                            'Word Length', 'Response Code', 'Sample Size'])
 
     return results
 
 
-def rolling_avg(values: np.ndarray, period: int) -> np.ndarray:
-    period = min(period, len(values))
-    smoothed = values.copy()
-    for i in range(1, period):
-        smoothed[i] = np.mean(values[:period + i])
-    for i in range(period, len(values)):
-        if values[i] > 0:
-            smoothed[i] = np.mean(values[i - period: i])
-        else:
-            smoothed[i] = smoothed[i - 1]
+def moving_avg(values: np.ndarray, interval: int, front_trim=20, end_trim=0) -> np.ndarray:
+    """
+    Since the values near the endpoints are not fully averaged, there are optional arguments
+    to remove them from the final result. In the typical use case of a subreddit
+    succumbing to the unwashed masses, the values near the endpoint are less prone to noise,
+    so it defaults to 0.
+    """
 
-    return smoothed
+    smoothed = np.zeros(len(values))
+    for i in range(interval):
+        smoothed[i] = np.mean(values[:i])
+    for i in range(interval, len(values)):
+        smoothed[i] = np.mean(values[i - interval : i])
+
+    return smoothed[front_trim : len(values) - end_trim]
 
 
 def make_plots(results: pd.DataFrame, subreddit: str, smoothing=50) -> None:
-
     # Remove all unsuccsesful requests
     cleaned_results = results[results['Response Code'] == 200]
 
@@ -86,7 +103,7 @@ def make_plots(results: pd.DataFrame, subreddit: str, smoothing=50) -> None:
 
     plt.figure()
     plt.title(f'Average /r/{subreddit} Comment Length')
-    plt.plot(rolling_avg(cleaned_results['Comment Length'].values, smoothing))
+    plt.plot(moving_avg(cleaned_results['Comment Length'].values, smoothing))
     ax = plt.axes()
     ax.set_xticks([0, int(len(dates)/3), 2*int(len(dates)/3), len(dates)-1])
     ax.set_xticklabels([dates[0], dates[int(len(dates)/3)],
@@ -96,7 +113,7 @@ def make_plots(results: pd.DataFrame, subreddit: str, smoothing=50) -> None:
 
     plt.figure()
     plt.title(f'Average /r/{subreddit} Word Length')
-    plt.plot(rolling_avg(cleaned_results['Word Length'].values, smoothing), 'red')
+    plt.plot(moving_avg(cleaned_results['Word Length'].values, smoothing), 'red')
     ax = plt.axes()
     ax.set_xticks([0, int(len(dates)/3), 2*int(len(dates)/3), len(dates)-1])
     ax.set_xticklabels([dates[0], dates[int(len(dates)/3)],
@@ -108,20 +125,19 @@ def make_plots(results: pd.DataFrame, subreddit: str, smoothing=50) -> None:
 if __name__ == '__main__':
 
     SUBREDDIT = 'pics'  # no slashes
-    SAMPLE_SIZE = 300  # Number of comments per day, maximum 500
-    NUMBER_OF_DAYS = 800  # Total processing time will require about 1 sec per day
+    SAMPLE_SIZE = 100  # Number of comments per day, maximum 100
+    NUMBER_OF_DAYS = 850  # Total processing time will require about 1 sec per day
+
     start_time = int(time.time()) - NUMBER_OF_DAYS * 86400
-
-
-    # results = analyze(start_time, SUBREDDIT, SAMPLE_SIZE)
-    # results.to_csv(f'{SUBREDDIT}_analysis.csv')
-    # make_plots(results, SUBREDDIT)
+    results = analyze(start_time, SUBREDDIT, SAMPLE_SIZE, small_sample_regime=100)
+    results.to_csv(f'{SUBREDDIT}_analysis.csv')
+    make_plots(results, SUBREDDIT, smoothing=75)
 
 
     # For replotting without running the whole thing again
 
-    results = pd.read_csv('{SUBREDDIT}_analysis.csv')
-    make_plots(results, SUBREDDIT,smoothing=100)
+    # results = pd.read_csv(f'{SUBREDDIT}_analysis.csv')
+    # make_plots(results, SUBREDDIT,smoothing=70)
 
 
 
